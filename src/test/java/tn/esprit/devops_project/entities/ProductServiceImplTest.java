fb90cb19d5e38abf89801898fa6afbf25eee07be
MeriamEsprit <meriem.hjiri@esprit.tn>
2024-03-25 04:53:25 +0100

    kkkkh3

--- src/test/java/tn/esprit/devops_project/entities/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/entities/ProductServiceImplTest.java
@@ -36,7 +36,9 @@ class ProductServiceImplTest {
 
         Stock stock = new Stock(1L,"BERSHKA",null);
         Stock savedStock = stockRepository.save(stock);
-        /*Product product = new Product(6L,"T-shirt",320,50,ProductCategory.CLOTHING,null);
+
+        assertNotNull(savedStock);
+            /*Product product = new Product(6L,"T-shirt",320,50,ProductCategory.CLOTHING,null);
         Product savedProduct = iProductService.addProduct(product, 1L);
         //productRepository.save(savedProduct);
         Stock savedStock = stockRepository.findById(savedProduct.getStock().getIdStock()).orElse(null);
@@ -44,7 +46,6 @@ class ProductServiceImplTest {
         assertNotNull(savedProduct);
         Product retrievedProduct = productRepository.findById(savedProduct.getIdProduct()).orElse(null);
         */
-        assertNotNull(savedStock);
 
 
     }
