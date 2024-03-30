58f2faaf2678527f698e4e32e53602a459e16c81
MeriamEsprit <meriem.hjiri@esprit.tn>
2024-03-30 04:26:30 +0100

    test

--- src/test/java/tn/esprit/devops_project/entities/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/entities/ProductServiceImplTest.java
@@ -33,62 +33,21 @@ class ProductServiceImplTest {
     private IProductService productService;
 
     @Test
-    public void AddProductTest() {
+    public void testAddProduct() {
+        Stock stock = stockRepository.findAll().get(0);
+        Product newProduct = new Product();
+        newProduct.setTitle("New Product");
+        newProduct.setPrice(20.0f);
+        newProduct.setQuantity(50);
+        newProduct.setCategory(ProductCategory.CLOTHING);
+        productService.addProduct(newProduct, stock.getIdStock());
 
-        Product product1 = new Product();
-        product1.setTitle("Test Product 1");
-        product1.setPrice(10.0f);
-        product1.setQuantity(5);
-        product1.setCategory(ProductCategory.ELECTRONICS);
-
-        Product product2 = new Product();
-        product2.setTitle("Test Product 2");
-        product2.setPrice(20.0f);
-        product2.setQuantity(8);
-        product2.setCategory(ProductCategory.CLOTHING);
-
-        Product product3 = new Product();
-        product3.setTitle("Test Product 3");
-        product3.setPrice(30.0f);
-        product3.setQuantity(10);
-        product3.setCategory(ProductCategory.BOOKS);
-
-        Product addedProduct1 = productService.addProduct(product1, 1L);
-        Product addedProduct2 = productService.addProduct(product2, 2L);
-        Product addedProduct3 = productService.addProduct(product3, 3L);
-
-        assertNotNull(addedProduct1);
-        assertNotNull(addedProduct2);
-        assertNotNull(addedProduct3);
-
-        assertEquals("Test Product 1", addedProduct1.getTitle());
-        assertEquals("Test Product 2", addedProduct2.getTitle());
-        assertEquals("Test Product 3", addedProduct3.getTitle());
-    }
-    @Test
-    public void retrieveAllProductsTest(){
-        List<Product> products = productService.retreiveAllProduct();
-        assertNotNull(products);
-    }
-
-    @Test
-    public void retrieveProductTest() {
-
-        long expectedProductId = 17L;
-
-        Product savedProduct = productService.retrieveProduct(expectedProductId);
-        assertNotNull(savedProduct);
-
-        assertEquals(expectedProductId, savedProduct.getIdProduct());
-    }
-
-    @Test
-    public void retrieveProductByCategoryTest() {
-        ProductCategory category = ProductCategory.ELECTRONICS;
-
-        List<Product> products = productService.retrieveProductByCategory(category);
-
-        assertNotNull(products);
+        Product retrievedProduct = productRepository.findById(newProduct.getIdProduct()).orElse(null);
+        assertNotNull(retrievedProduct);
+        assertEquals("New Product", retrievedProduct.getTitle());
+        assertEquals(20.0f, retrievedProduct.getPrice());
+        assertEquals(50, retrievedProduct.getQuantity());
+        assertEquals(ProductCategory.CLOTHING, retrievedProduct.getCategory());
     }
 
 }
